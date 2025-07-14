e04becb111bd1fdde26a7b54d0b17ebbb4cb8a73
Jean Pierre <jalvarezidep@gmail.com>
2025-07-14 13:50:03 -0500

    First Hola mundo

---
+++ hola-mundo/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Hola from './Hola.jsx'
+
+function App() {
+  return (
+    <Hola></Hola>
+  )
+}
+
+export default App
---
+++ hola-mundo/src/Hola.jsx
@@ -0,0 +1,8 @@
+
+function Hola(){
+    return(
+        <h1>Hola, mundo</h1>
+    )
+}
+
+export default Hola;
