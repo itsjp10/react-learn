d86119d2068c0dd8e8813cd3ee2a50ed41913098
Jean Pierre <jalvarezidep@gmail.com>
2025-07-14 14:50:32 -0500

    Hola mundo + counter

--- hola-mundo/src/App.jsx
+++ hola-mundo/src/App.jsx
@@ -1,18 +1,27 @@
 import './App.css'
 import Hola from './Hola.jsx'
+import Counter from './Counter.jsx'
 import { useState } from 'react'
 
 function App() {
   const [person, setPerson] = useState("Mundo")
+  const [count, setCount] = useState(0);
   
-  const handleClick = () =>{
+  const changeName = () =>{
     setPerson(person == "Mundo" ? "Jean" : "Mundo");
   }
 
   return (
     <>
+      {/*primer proyecto*/}
       <Hola person={person}></Hola>
-      <button onClick={handleClick}></button>
+      <button onClick={changeName}></button>
+
+      {/*contador*/}
+      <Counter count={count}></Counter>
+      <button onClick={() => setCount(count-1)}>-</button>
+      <button onClick={()=> setCount(0)}>Reset</button>
+      <button onClick={()=> setCount(count+1)}>+</button>
     </>
   )
 }
---
+++ hola-mundo/src/Counter.jsx
@@ -0,0 +1,8 @@
+import { Children } from "react";
+
+function Counter({ count }){ 
+    return(
+        <h1>Contador: {count}</h1>
+    )
+}
+export default Counter;
