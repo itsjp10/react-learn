49fd8a98979aa185b0e2e6cde4660ab93adc4662
Jean Pierre <jalvarezidep@gmail.com>
2025-07-19 12:39:25 -0500

    fetch de datos creado con setInteval de 5 segundos para fetching + cleanup function to avoid leaks

--- hola-mundo/src/App.jsx
+++ hola-mundo/src/App.jsx
@@ -3,18 +3,34 @@ import { useState, useEffect } from 'react'
 
 function App() {
   const [data, setData] = useState(null)
+  const [option, setOption] = useState('')
 
-  const handleUsers = () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(json => setData(json))
-  }
+  useEffect(() => {
+    const fetchData = () => {
+      fetch(`https://jsonplaceholder.typicode.com/${option}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Respuesta no válida del servidor');
+          }
+          return response.json();
+        })
+        .then(json => setData(json))
+        .catch(err => console.error('Error al obtener datos:', err));
+      console.log('fetching data for ',option)
+    }
+    fetchData(); // Primer fetch inmediato
+    const intervalId = setInterval(fetchData, 5000)
 
+    //Cleanup function
+    return () => clearInterval(intervalId)
+  }, [option])
 
   return (
     <>
       <div>
-        <button onClick={handleUsers} type='button'>Users</button>
+        <button onClick={() => { setOption('users') }} type='button'>Users</button>
+        <button onClick={() => { setOption('posts') }} type='button'>Posts</button>
+        <button onClick={() => { setOption('comments') }} type='button'>Comments</button>
         <div>
           {data && (data.map((user, i) => (
 
